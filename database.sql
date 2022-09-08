9db7833473277287be8f7b6bb0c977da558d0a6d
YARDELOPE <yardel.lopez@utelvt.edu.ec>
2022-09-08 14:20:02 -0500

    Cree una base de datos

---
+++ database.sql
@@ -0,0 +1,3 @@
+create database miplataforma;
+use miplataforma;
+create table persona(idpersona int, nombres varchar(20), cedula varchar(10), idgenero int, fechanacimiento date, idnacionalidad int, idetnia int, idestadocivil int);
